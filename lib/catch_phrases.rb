433fe7c6852bcc7f7de52d202f0f28f3d2c62b05
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 02:18:40 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,15 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+'''ruby
+
+def mario_plumber
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  phrase = "Its a me Mario!"
+  puts status
+end
+#mario_plumber
+
+def mario_plumber
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  phrase = "Its a me Mario!"
   puts phrase
 end
 
